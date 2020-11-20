8dda1b39ea6ae4cd040cda67b76e596a9ad435d4
vinczeadam91 <vinczeadam91@users.noreply.github.com>
2020-11-20 02:18:05 +0100

    Mobil, animálás van hátra

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const showModalBtn = document.querySelector('.body__showModalBtn');
+
+const modalClose = document.querySelector('#closeModal');
+
+const modalContainer = document.querySelector('.body__modalContainer');
+
+const modal = document.querySelector('.modalContainer__modal');
+
+const modalOkeyButton = document.querySelector('.modal__footerButton--green');
+
+const modalCancelButton = document.querySelector('.modal__footerButton--red');
+
+showModalBtn.addEventListener("click", ()=>{
+    modalContainer.classList.add('body__modalContainer--active');
+})
+
+modalClose.addEventListener("click", ()=>{
+    modalContainer.classList.remove('body__modalContainer--active');
+})
+
+modalOkeyButton.addEventListener("click", ()=>{
+    modalContainer.classList.remove('body__modalContainer--active');
+})
+
+modalCancelButton.addEventListener("click", ()=>{
+    modalContainer.classList.remove('body__modalContainer--active');
+})
+
+modalContainer.addEventListener("click", ()=>{
+    modalContainer.classList.remove('body__modalContainer--active');
+})
